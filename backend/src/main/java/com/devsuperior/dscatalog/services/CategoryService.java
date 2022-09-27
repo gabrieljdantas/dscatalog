d7d30f108e8e8327a2f0f04c1de2d6289eee39eb
Gabriel Jesus Dantas <gabrielrpg68@gmail.com>
2022-09-27 00:48:24 -0300

    POST category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -32,5 +32,14 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDto(entity);
 	}
+
+	public CategoryDto insert(CategoryDto dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDto(entity);
+	}
 	
 }
